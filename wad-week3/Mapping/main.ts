64ec4b576be0a2efa4a0df9b0f4647b04b2e1460
Kenny Bello <2bellk09@solent.ac.uk>
2026-02-12 15:10:43 +0000

    mapping with leaflet,live sever and vite

---
+++ wad-week3/Mapping/main.ts
@@ -0,0 +1,12 @@
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+const map = L.map ("map1");
+
+const attrib="Map data copyright OpenStreetMap contributors, Open Database Licence";
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { 
+    attribution: attrib 
+}).addTo(map);
+            
+map.setView([50.908,-1.4], 14);
